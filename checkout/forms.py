b1d4c897d61379c5d37ff332f802e428fa935918
sruss07 <srussell1903@yahoo.co.uk>
2022-02-17 16:37:21 +0000

    updates to checkout forms.py file

--- checkout/forms.py
+++ checkout/forms.py
@@ -10,6 +10,7 @@ class OrderForm(forms.ModelForm):
                   'town_or_city', 'postcode', 'country',
                   'county',)
 
+
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
@@ -26,6 +27,7 @@ class OrderForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'county': 'County, State or Locality',
         }
+
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
