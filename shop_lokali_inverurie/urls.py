1ba4d4ebcdab3faf11aecd22a4101d0f3683c539
sruss07 <srussell1903@yahoo.co.uk>
2022-02-18 14:25:57 +0000

    updated url paths in root urls.py file

--- shop_lokali_inverurie/urls.py
+++ shop_lokali_inverurie/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('review/', include('review.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
