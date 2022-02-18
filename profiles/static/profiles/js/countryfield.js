25c2a00d7b8dc5abbc96a70abdf3d08a72d2c729
sruss07 <srussell1903@yahoo.co.uk>
2022-02-18 11:15:14 +0000

    updates to profiles app

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,12 +1,72 @@
-let countrySelected = $('#id_default_country').val();
-if(!countrySelected) {
-    $('#id_default_country').css('color', '#aab7c4');
-};
-$('#id_default_country').change(function() {
-    countrySelected = $(this).val();
-    if(!countrySelected) {
-        $(this).css('color', '#aab7c4');
-    } else {
-        $(this).css('color', '#000');
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
     }
+};
+var card = elements.create('card', { style: style });
+card.mount('#card-element');
+
+
+// Realtime card validation errors
+// Code from CI Stripe lesson
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+            `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Form submit
+// Code from CI Stripe lesson and Stripe documentation
+var form = document.getElementById('checkout-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    $('#submit-button').attr('disabled', true);
+    $('#loading-overlay').fadeToggle(100);    
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#loading-overlay').fadeToggle(100);               
+            card.update({ 'disabled': false });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
