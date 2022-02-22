bd7484c7b5b5e8bdfdd3a4b410aa6459385c72b8
sruss07 <srussell1903@yahoo.co.uk>
2022-02-22 14:57:23 +0000

    ipdates to settings.py

--- shop_lokali_inverurie/settings.py
+++ shop_lokali_inverurie/settings.py
@@ -7,12 +7,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 import dj_database_url
 
-if os.path.exists("env.py"):
-    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
